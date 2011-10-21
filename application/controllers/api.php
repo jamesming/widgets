1e183dcfe5f0f2e65130563d04647ba3ba0ada69
James Ming <jamesming@gmail.com>
2011-10-21 10:03:49 -0700

    added

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -1,6 +1,9 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Api extends CI_Controller {
+
+
+
+// http://mynuvotv.com/api/v1/getScheduleByDateRange?start_date=2011-10-16&end_date=2011-10-24class Api extends CI_Controller {
 	
 	
    public function __construct(){
