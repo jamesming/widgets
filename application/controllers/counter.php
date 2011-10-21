4e4de219e6640f39900c4cd6428041176e6d9100
James Ming <jamesming@gmail.com>
2011-10-21 13:57:26 -0700

    added

--- application/controllers/counter.php
+++ application/controllers/counter.php
@@ -42,6 +42,31 @@ class Counter extends CI_Controller {
 		 
 	}
 	
+	
+	
+	
+	public function count_adbuys(){
+		
+		
+					$this->load->library('curl');
+					$geo_location =  $this->curl->simple_get('http://api.ipinfodb.com/v3/ip-city/?key=a644434b1b3c5ccc56d42931601df57c3ca668e40cb5bcc81be426e87ca10f51&ip=' . $_SERVER['REMOTE_ADDR']);
+												
+					$geo_array = explode(';',$geo_location);
+	
+						$this->custom->insert_page_views( 
+							array(
+								'table' => 'page_views_adbuys',
+								'ip_address' => $geo_array[2],
+								'country' => $geo_array[4],
+								'state' => $geo_array[5],
+								'city' => $geo_array[6]
+								)
+						 );
+		
+		 
+	}
+	
+	
 
 	/**
 	 * report.
