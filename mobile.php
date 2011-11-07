3078ba405c286ca3071565e3d973ad3439fb9f8d
James Ming <jamesming@gmail.com>
2011-11-07 13:56:41 -0800

    added

--- mobile.php
+++ mobile.php
@@ -14,6 +14,101 @@ $db = mysql_connect($host,$user,$pw)
 
 mysql_select_db($database,$db)
     or die("Cannot connect to database.");
+    
+    
+    
+class array2xml extends DomDocument
+{
+
+    public $nodeName;
+
+    private $xpath;
+
+    private $root;
+
+    private $node_name;
+
+
+    /**
+    * Constructor, duh
+    *
+    * Set up the DOM environment
+    *
+    * @param    string    $root        The name of the root node
+    * @param    string    $nod_name    The name numeric keys are called
+    *
+    */
+    public function __construct($root='root', $node_name='node')
+    {
+        parent::__construct();
+
+        /*** set the encoding ***/
+        $this->encoding = "ISO-8859-1";
+
+        /*** format the output ***/
+        $this->formatOutput = true;
+
+        /*** set the node names ***/
+        $this->node_name = $node_name;
+
+        /*** create the root element ***/
+        $this->root = $this->appendChild($this->createElement( $root ));
+
+        $this->xpath = new DomXPath($this);
+    }
+
+    /*
+    * creates the XML representation of the array
+    *
+    * @access    public
+    * @param    array    $arr    The array to convert
+    * @aparam    string    $node    The name given to child nodes when recursing
+    *
+    */
+    public function createNode( $arr, $node = null)
+    {
+        if (is_null($node))
+        {
+            $node = $this->root;
+        }
+        foreach($arr as $element => $value) 
+        {
+            $element = is_numeric( $element ) ? $this->node_name : $element;
+
+            $child = $this->createElement($element, (is_array($value) ? null : $value));
+            $node->appendChild($child);
+
+            if (is_array($value))
+            {
+                self::createNode($value, $child);
+            }
+        }
+    }
+    /*
+    * Return the generated XML as a string
+    *
+    * @access    public
+    * @return    string
+    *
+    */
+    public function __toString()
+    {
+        return $this->saveXML();
+    }
+
+    /*
+    * array2xml::query() - perform an XPath query on the XML representation of the array
+    * @param str $query - query to perform
+    * @return mixed
+    */
+    public function query($query)
+    {
+        return $this->xpath->evaluate($query);
+    }
+
+} // end of class
+
+
  
 if( isset($_GET['date']) ){
 	
@@ -30,8 +125,9 @@ if( isset($_GET['date']) ){
 };
 
 $query = 	"SELECT
-					 	carousel_items_sets.id  AS carousel_items_sets_id,
+						carousel_items_sets.id  AS carousel_items_sets_id,
 					 	carousel_items.name AS 	carousel_items_name,
+					 	carousel_items.iphone_directTo,
 					 	carousel_items_images.id AS carousel_items_image_id
 					 FROM 
 					 	carousel_sets_calendars,
@@ -141,30 +237,54 @@ foreach( $sets  as  $set){
 };
 
 
+//echo '<pre>';print_r( $carousel_items  );echo '</pre>';  exit;
+
 foreach( $carousel_items  as $carousel_item){
-		foreach(  $carousel_item as  $key => $images){
+		foreach(  $carousel_item as  $key => $values){
 			if( $key == 'images'){
-				foreach( $images  as $carousel_items_array){ 
+				foreach( $values  as $carousel_items_array){ 
 					foreach( $carousel_items_array  as  $carousel_items_id){
 						$container[] = 'http://cms.mynuvotv.com/uploads/carousel_items_images/'.$carousel_items_id.'/image.png';
 					}
 				}
 			};
+			
+			if( $key == 'carousel_items_name' ||
+					$key == 'iphone_directTo'
+			){
+				$container[] =$values;
+			};			
+			
 		}
+
 		$shows[] = $container;
 		unset($container);
 }
 
-if( isset($_GET["json"]) && $_GET["json"] == 'true'){
+if( isset($_GET["output"]) && $_GET["output"] == 'json'){
 	
 		header('Cache-Control: no-cache, must-revalidate');
 		header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 		header('Content-type: application/json');
 		echo json_encode($shows);
 	
-}else{
+}elseif( isset($_GET["output"]) && $_GET["output"] == 'xml'){
+
+		try 
+		{
+		    $xml = new array2xml('my_node');
+		    $xml->createNode( $shows );
+		    echo  $xml ;
+		} 
+		catch (Exception $e) 
+		{
+		    echo $e->getMessage();
+		}
+
+
+}elseif( isset($_GET["output"]) && $_GET["output"] == 'test'){
 	
-	echo '<pre>';print_r(  $shows  );echo '</pre>';  exit;
+		echo '<pre>';print_r(  $shows  );echo '</pre>';  exit;
 	
 };
 
