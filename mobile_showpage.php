312b6ad24c950a8c472f950169d7ff7c6092706a
James Ming <jamesming@gmail.com>
2011-11-08 13:00:56 -0800

    added

--- mobile_showpage.php
+++ mobile_showpage.php
@@ -35,7 +35,6 @@ $query = 	"SELECT
 						carousel_items_sets.id  AS carousel_items_sets_id,
 					 	carousel_items.name AS 	carousel_items_name,
 					 	carousel_items.iphone_directTo,
-					 	carousel_items_images.image_type,
 					 	carousel_items_images.id AS carousel_items_image_id
 					 FROM 
 					 	carousel_sets_calendars,
@@ -84,12 +83,14 @@ while ($row = mysql_fetch_assoc($result)) {
 	$sets[] = $row;
 }
 
-//echo '<pre>';print_r( $sets   );echo '</pre>';  exit;
+
+echo '<pre>';print_r(  $sets  );echo '</pre>';  exit;
+
 
 $previous_id = "";
 
 $count=0;
-foreach( $sets  as  $key =>  $set){
+foreach( $sets  as  $set){
 	$count++;
 	if( $previous_id == $set['carousel_items_sets_id']	||
 			$previous_id == ""
@@ -146,30 +147,24 @@ foreach( $sets  as  $key =>  $set){
 };
 
 
-$image_types = array('hero_iphone', 'right_tab_iphone', 'right_tab_border_iphone');
+//echo '<pre>';print_r( $carousel_items  );echo '</pre>';  exit;
 
 foreach( $carousel_items  as $carousel_item){
-		foreach(  $carousel_item as  $key0 => $values){
-			if( $key0 == 'images'){
-				$count=0;
-				foreach( $values  as $key1 => $carousel_items_array){ 
-					
-					
-					foreach( $carousel_items_array  as  $key2 => $carousel_items_id){
-						$container[  $image_types[$count]   ] = 'http://cms.mynuvotv.com/uploads/carousel_items_images/'.$carousel_items_id.'/image.png';
-						$count++;
+		foreach(  $carousel_item as  $key => $values){
+			if( $key == 'images'){
+				foreach( $values  as $carousel_items_array){ 
+					foreach( $carousel_items_array  as  $carousel_items_id){
+						$container[] = 'http://cms.mynuvotv.com/uploads/carousel_items_images/'.$carousel_items_id.'/image.png';
 					}
 				}
 			};
 			
-			if( $key0 == 'iphone_directTo'
+			if( $key == 'carousel_items_name' ||
+					$key == 'iphone_directTo'
 			){
-				$container[$key0] =$values;
+				$container[] =$values;
 			};			
-			if( $key0 == 'carousel_items_name'
-			){
-				$container[$key0] =$values;
-			};				
+			
 		}
 
 		$shows[] = $container;
