257f8b94354094b1f9ff23b0574432f46c53c9eb
James Ming <jamesming@gmail.com>
2011-11-10 10:38:21 -0800

    added

--- mobile.php
+++ mobile.php
@@ -14,17 +14,15 @@ $db = mysql_connect($host,$user,$pw)
 
 mysql_select_db($database,$db)
     or die("Cannot connect to database.");
-    
-include('convert_array_xml_class.php');
  
 if( isset($_GET['date']) ){
-	
+
 		$date_array = explode('-', $_GET['date']);
 		$year_to_view = $date_array[0];
 		$year_to_view_minus_one_year = $year_to_view - 1;
-	 
+
 		$day_to_view = date('z',  strtotime($_GET['date']));
-		
+
 }else{
 		$year_to_view = date('Y');
 		$year_to_view_minus_one_year = $year_to_view - 1;
@@ -32,10 +30,8 @@ if( isset($_GET['date']) ){
 };
 
 $query = 	"SELECT
-						carousel_items_sets.id  AS carousel_items_sets_id,
+					 	carousel_items_sets.id  AS carousel_items_sets_id,
 					 	carousel_items.name AS 	carousel_items_name,
-					 	carousel_items.iphone_directTo,
-					 	carousel_items_images.image_type,
 					 	carousel_items_images.id AS carousel_items_image_id
 					 FROM 
 					 	carousel_sets_calendars,
@@ -84,123 +80,92 @@ while ($row = mysql_fetch_assoc($result)) {
 	$sets[] = $row;
 }
 
-//echo '<pre>';print_r( $sets   );echo '</pre>';  exit;
 
 $previous_id = "";
 
 $count=0;
-foreach( $sets  as  $key =>  $set){
+foreach( $sets  as  $set){
 	$count++;
 	if( $previous_id == $set['carousel_items_sets_id']	||
 			$previous_id == ""
 			){
-		
+
 			foreach( $set  as  $field => $value){
-				
+
 				$carousel_item[$field] = $set[$field];
-				
+
 				if( $field == 'carousel_items_image_id' 
 				){
 						$array[$field] = $value;
 				};
-				
+
 				if( isset($array) && count($array) == 1 ){
 						$image[] = $array;
 						unset($array);			
 				};
 
-				
+
 			};	
 		$carousel_item['images'] = $image;			
-		
+
 	}else{
 		$carousel_items[]=$carousel_item;			
 		unset($image);
 
 			foreach( $set  as  $field => $value){
-				
+
 				$carousel_item[$field] = $set[$field];
-				
+
 				if( $field == 'carousel_items_image_id' 
 				){
 						$array[$field] = $value;
 				};
-				
+
 				if(  isset($array) && count($array) == 1 ){
 						$image[] = $array;
 						unset($array);			
 				};
-				
+
 			};					
-		
+
 	};
-	
-	
+
+
 	if( $count ==  count($sets) ){
-		
+
 		$carousel_items[]=$carousel_item;
 
 	};
-	
+
 	$previous_id = $set['carousel_items_sets_id'];		
 };
 
 
-$image_types = array('hero_iphone', 'right_tab_iphone', 'right_tab_border_iphone');
-
 foreach( $carousel_items  as $carousel_item){
-		foreach(  $carousel_item as  $key0 => $values){
-			if( $key0 == 'images'){
-				$count=0;
-				foreach( $values  as $key1 => $carousel_items_array){ 
-					
-					
-					foreach( $carousel_items_array  as  $key2 => $carousel_items_id){
-						$container[  $image_types[$count]   ] = 'http://cms.mynuvotv.com/uploads/carousel_items_images/'.$carousel_items_id.'/image.png';
-						$count++;
+		foreach(  $carousel_item as  $key => $images){
+			if( $key == 'images'){
+				foreach( $images  as $carousel_items_array){ 
+					foreach( $carousel_items_array  as  $carousel_items_id){
+						$container[] = 'http://cms.mynuvotv.com/uploads/carousel_items_images/'.$carousel_items_id.'/image.png';
 					}
 				}
 			};
-			
-			if( $key0 == 'iphone_directTo'
-			){
-				$container[$key0] =$values;
-			};			
-			if( $key0 == 'carousel_items_name'
-			){
-				$container[$key0] =$values;
-			};				
 		}
-
 		$shows[] = $container;
 		unset($container);
 }
 
-if( isset($_GET["output"]) && $_GET["output"] == 'json'){
-	
+if( isset($_GET["json"]) && $_GET["json"] == 'true'){
+
 		header('Cache-Control: no-cache, must-revalidate');
 		header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 		header('Content-type: application/json');
 		echo json_encode($shows);
-	
-}elseif( isset($_GET["output"]) && $_GET["output"] == 'xml'){
 
-		try 
-		{
-		    $xml = new array2xml('my_node');
-		    $xml->createNode( $shows );
-		    echo  $xml ;
-		} 
-		catch (Exception $e) 
-		{
-		    echo $e->getMessage();
-		}
+}else{
 
+	echo '<pre>';print_r(  $shows  );echo '</pre>';  exit;
 
-}elseif( isset($_GET["output"]) && $_GET["output"] == 'test'){
-	
-		echo '<pre>';print_r(  $shows  );echo '</pre>';  exit;
-	
 };
 
 
