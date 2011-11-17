2d0fae7ee02aaf7d3fdc96b015c79a0db98c56f2
James Ming <jamesming@gmail.com>
2011-11-16 20:46:44 -0800

    added

--- mobile_class.php
+++ mobile_class.php
@@ -195,7 +195,7 @@ class Mobile_api{
 										 AND
 										 	showpage_cast_items.id = showpage_cast_items_images.showpage_cast_item_id
 										 AND
-										 	showpage_cast_items_images.image_type_id in (22)
+										 	showpage_cast_items_images.image_type_id in (13)
 										 ";
 
 				
