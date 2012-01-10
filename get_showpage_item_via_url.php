d16302d9d4f40910532cd69be06686f1ec2c008d
James Ming <jamesming@gmail.com>
2012-01-10 11:08:35 -0800

    added

---
+++ get_showpage_item_via_url.php
@@ -0,0 +1,21 @@
+<?php     
+    
+include('db_connect.php');
+
+$query = 	"SELECT id
+					 FROM 
+					 	showpage_items
+					 WHERE
+					 	url_name = ".$get['url_name']."
+				 ";
+
+$result = mysql_query($query);
+while ($row = mysql_fetch_assoc($result)) {
+	foreach( $row  as  $key => $value){
+		$showpage_item[$key] = $value;
+	}
+	$showpages_items[] = $showpage_item;
+}
+
+echo '<pre>';print_r(  $showpages_items  );echo '</pre>';  exit;
+?>
